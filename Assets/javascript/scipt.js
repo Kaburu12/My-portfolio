d1ca94a5b87589ba7a819442c8f5f70177a81f48
Kaburu12 <michael.kaburur@student.moringaschool.com>
2022-06-06 21:42:32 +0300

    create slides on my project section

---
+++ Assets/javascript/scipt.js
@@ -0,0 +1,22 @@
+//slideshow functions
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function nextSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slideshow");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
